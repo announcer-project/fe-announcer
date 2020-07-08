b68054caae6c54487936d166007dc0b747a1f84c
panupongjkn <panupong.jkn@gmail.com>
2020-07-08 22:42:36 +0700

    [UPDATE] update .env

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    REACT_APP_CHANNEL_ID_LOGIN: process.env.REACT_APP_CHANNEL_ID_LOGIN,
+    REACT_APP_FE_PATH: process.env.REACT_APP_FE_PATH,
+    REACT_APP_BE_PATH: process.env.REACT_APP_BE_PATH,
+    REACT_APP_STORAGE: process.env.REACT_APP_STORAGE,
+  },
+};
