6b5bd5197e1c8fbeda4c77c23c712ad6448edb24
panupongjkn <panupong.jkn@gmail.com>
2020-07-25 10:44:34 +0700

    [ADD] add css of next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS({
   env: {
     REACT_APP_CHANNEL_ID_LOGIN: process.env.REACT_APP_CHANNEL_ID_LOGIN,
     REACT_APP_FE_PATH: process.env.REACT_APP_FE_PATH,
     REACT_APP_BE_PATH: process.env.REACT_APP_BE_PATH,
     REACT_APP_STORAGE: process.env.REACT_APP_STORAGE,
   },
-};
+});
