54aa1c22e535e58ad368a5dd14e0d29483442671
panupongjkn <panupong.jkn@gmail.com>
2020-08-04 21:38:43 +0700

    [UPDATE] edit style page and using be api

--- components/Register/RegisterPage.js
+++ components/Register/RegisterPage.js
@@ -1,56 +1,53 @@
-import Layouts from "../Layouts/Layouts"
-import Step1 from "./Step1"
-import Step2 from "./Step2"
-import Step3 from "./Step3"
-import { Steps } from 'antd';
+import Layouts from "../Layouts/Layouts";
+import Step1 from "./Step1";
+import StepConnectSocial from "./Step1-2";
+import Step2 from "./Step2";
+import Step3 from "./Step3";
+import { Steps } from "antd";
 import React, { useContext } from "react";
 import { RegisterContext } from "../../store/RegisterProvider";
 import styled from "styled-components";
 import "./Register.module.css";
 
-
 const Box = styled.div`
-    border: 2px solid white;
-    padding: 10px;
-    border-radius: 10px;
-    box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.25);
-`
+  border: 2px solid white;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.25);
+`;
 
 const { Step } = Steps;
 
 export default function RegisterPage(props) {
-    const { step } = useContext(RegisterContext);
-    const StepRegister = () => {
-        switch (step) {
-            case 1:
-                return <Step1 {...props} />;
-            case 2:
-                return <Step2 {...props} />;
-            case 3:
-                return <Step3 {...props} />;
-        }
-    };
+  const { step } = useContext(RegisterContext);
+  const StepRegister = () => {
+    switch (step) {
+      case 1:
+        return <Step1 {...props} />;
+      case 1.2:
+        return <StepConnectSocial {...props} />;
+      case 2:
+        return <Step2 {...props} />;
+      case 3:
+        return <Step3 {...props} />;
+    }
+  };
 
-    return (
-        <Layouts {...props}>
-            <div className="container">
-                <div className="col-12">
-                    <Box className="col-8 px-5 mt-5 mx-auto p-4">
-                        <h1 className="text-center">Register</h1>
-                        <div className="col-10 mx-auto mt-5">
-                            <Steps current={step - 1}>
-                                <Step title="Verify your e-mail" />
-                                <Step title="In Progress" />
-                                <Step title="Waiting" />
-                            </Steps>,
-                        </div>
-                        <div className="col-6 mx-auto">
-                            {StepRegister()}
-                        </div>
-                    </Box>
-                </div>
-            </div>
-                    <div className="fomt-colorred">hi</div>
-        </Layouts>
-    )
+  return (
+    <Layouts {...props}>
+      <div className="col-12">
+        <Box className="col-8 my-5 mx-auto py-5">
+          <p className="text-center font-title m-0" >Register</p>
+          <div id="StepRegister" className="col-10 mx-auto mt-4">
+            <Steps size="small" current={step - 1}>
+              <Step title="Verify your e-mail" />
+              <Step title="In Progress" />
+              <Step title="Confirm" />
+            </Steps>
+          </div>
+          <div className="col-6 mx-auto mt-4">{StepRegister()}</div>
+        </Box>
+      </div>
+    </Layouts>
+  );
 }
--- pages/register/index.js
+++ pages/register/index.js
@@ -6,17 +6,23 @@ import { withNotAuth } from "../../tools/withNotAuth";
 import Page from "../../components/Register/RegisterPage";
 import { RegisterProvider } from "../../store/RegisterProvider";
 
-function RegisterPage() {
+function RegisterPage(props) {
   return (
     <>
       <Head>
         <title>NMS - Login</title>
       </Head>
       <RegisterProvider>
-          <Page/>
+        <Page {...props} />
       </RegisterProvider>
     </>
   );
+}
+
+export async function getServerSideProps(ctx) {
+  // await withNotAuth(ctx);
+  // const { code, state } = ctx.query;
+  return { props: { query: ctx.query } };
 }
 
 export default RegisterPage;
