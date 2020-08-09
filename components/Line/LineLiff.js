7972ecded2c1eb261cffc8427667072f4d9a992a
panupongjkn <panupong.jkn@gmail.com>
2020-08-09 22:17:55 +0700

    [UPDATE] test line login by liff

--- components/Line/LineLiff.js
+++ components/Line/LineLiff.js
@@ -8,6 +8,18 @@ export default function LiffInit() {
   const [version, setVersion] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [client, setClient] = useState("");
+  const [profile, setProfile] = useState({});
+
+  const getProfile = async () => {
+    if (liff.isLoggedIn()) {
+      console.log("login")
+      setProfile(await liff.getProfile());
+      console.log((await liff.getProfile()).displayName)
+    } else {
+      liff.login({ redirectUri: "http://localhost:3000/line" });
+    }
+  };
+
   useEffect(() => {
     liff.init({ liffId }).then(() => {
       setOS(liff.getOS());
@@ -15,6 +27,7 @@ export default function LiffInit() {
       setVersion(liff.getVersion());
       setAccessToken(liff.getAccessToken());
       setClient(liff.isInClient());
+      getProfile();
     });
   }, []);
   return (
@@ -23,7 +36,12 @@ export default function LiffInit() {
       <p>language: {language}</p>
       <p>version: {version}</p>
       <p>accessToken: {accessToken}</p>
-      <p>isClient: {client}</p>
+      <br />
+      <img src={profile.pictureUrl} />
+      <p>user id: {profile.userId}</p>
+      <p>user status: {profile.statusMessage}</p>
+      <p>user name: {profile.displayName}</p>
+      {/* <p>isClient: {client}</p> */}
     </div>
   );
 }
