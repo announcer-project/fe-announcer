7e90899b2e504b174fe9de64a74e0da316e3522c
christycrt <christycrt@gmail.com>
2020-09-16 16:34:09 +0700

    [UPDATE] Box connect social api

--- components/Console/System/Connect/AllConnectPage.js
+++ components/Console/System/Connect/AllConnectPage.js
@@ -1,9 +1,57 @@
-import React from "react"
+import React from "react";
+import Layout from "../Layout/Layout";
+import styled from "styled-components";
+import Button from "../../../../components/common/Button";
+
+const Box = styled.div`
+  border: 1px solid #a6a6a6;
+  cursor: pointer;
+  padding: 22px;
+  margin-bottom: 10px;
+`;
 
 export default function AllConnectPage() {
     return (
-        <div>
-            All connect page
-        </div>
+        <Layout>
+            <div className="container pt-4">
+                <h1>Connect social api</h1>
+                <Box>
+                    <div className="d-flex justify-content-between">
+                        <span className="mt-2">Line Official Account</span>
+                        <div>
+                            <Button
+                                danger={false}
+                            >
+                                Connect API
+                            </Button>
+                            <Button
+                                danger={true}
+                            >
+                                Disconnect
+                            </Button>
+                        </div>
+                    </div>
+
+                </Box>
+                <Box>
+                    <div className="d-flex justify-content-between">
+                        <span className="mt-2">Facebook Page</span>
+                        <div>
+                            <Button
+                                danger={false}
+                            >
+                                Connect API
+                            </Button>
+                            <Button
+                                danger={true}
+                            >
+                                Disconnect
+                            </Button>
+                        </div>
+                    </div>
+                </Box>
+            </div>
+        </Layout>
+
     )
 }
