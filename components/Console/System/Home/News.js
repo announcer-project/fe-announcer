33ae57948fda4ef04e21a376bc52f2c6c9d1f947
christycrt <christycrt@gmail.com>
2020-09-26 22:26:04 +0700

    [UPDATE] Show icon not have news

--- components/Console/System/Home/News.js
+++ components/Console/System/Home/News.js
@@ -2,6 +2,8 @@ import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
 import NewsCard from "../NewsPublishCard";
+import { Empty } from 'antd';
+import Button from "../../../common/Button";
 
 const SeeMore = styled.span`
   cursor: pointer;
@@ -26,17 +28,24 @@ export default function News({ news, query }) {
       </div>
       {news.length === 0 ? (
         <div className="border p-4 text-center font-small color-drop mt-2">
-          Not have news
+          <Empty
+            image={Empty.PRESENTED_IMAGE_SIMPLE}
+            description="Not have news"
+          >
+            <Link href={`/console/${query.systemname}/${query.systemid}/news/createnews`}>
+              <Button>Create news</Button>
+            </Link>
+          </Empty>
         </div>
       ) : (
-        <div className="col-12 mt-2">
-          <div className="row">
-            {news.map((news) => {
-              return <Link href={`/news/systemname/systemid/${news.ID}`}><NewsCard className="col-12 col-lg-4" news={news} /></Link>;
-            })}
+          <div className="col-12 mt-2">
+            <div className="row">
+              {news.map((news) => {
+                return <Link href={`/news/systemname/systemid/${news.ID}`}><NewsCard className="col-12 col-lg-4" news={news} /></Link>;
+              })}
+            </div>
           </div>
-        </div>
-      )}
+        )}
     </div>
   );
 }
