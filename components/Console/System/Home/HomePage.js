3eb8087d401bb70255d3bc20c9834b66269059cb
panupongjkn <panupong.jkn@gmail.com>
2020-10-25 01:06:26 +0700

    ♻️ use skeleton home page

--- components/Console/System/Home/HomePage.js
+++ components/Console/System/Home/HomePage.js
@@ -1,36 +1,60 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import cookie from "../../../../tools/cookie";
+import axios from "axios";
 
-import Layout from "../Layout/Layout";
 import News from "./News";
 import NewsTypes from "./NewsTypes";
 import TargerGroups from "./TargetGroups";
 
+import { NewsLoading, NewsTypesLoading, TargetgroupsLoading } from "./Skeleton";
+
 function HomeSystemPage(props) {
   const router = useRouter();
   const { systemname, systemid } = router.query;
+  const [news, setNews] = useState(null);
+  const [newstypes, setNewstypes] = useState(null);
+  const [targetgroups, setTagetgroups] = useState(null);
+
+  useEffect(() => {
+    fetchAboutSystem();
+  }, []);
+
+  const fetchAboutSystem = () => {
+    axios
+      .get(
+        `${process.env.REACT_APP_BE_PATH}/aboutsystem?systemid=${systemid}&systemname=${systemname}`,
+        {
+          headers: {
+            Authorization: "Bearer " + cookie.getJWT(),
+          },
+        }
+      )
+      .then((res) => {
+        let aboutSystem = res.data;
+        setNews(aboutSystem.news);
+        setNewstypes(aboutSystem.newstypes);
+        setTagetgroups(aboutSystem.targetgroups);
+      });
+  };
 
   return (
     <>
       <div className="container py-3">
         <h1>{systemname}</h1>
-        {props.news ? (
-          <News news={props.news} systemname systemid />
-        ) : (
-          <p>loading</p>
-        )}
+        {news ? <News news={news} systemname systemid /> : <NewsLoading />}
         <div className="mt-3">
-          {props.newstypes ? (
-            <NewsTypes newstypes={props.newstypes} />
+          {newstypes ? (
+            <NewsTypes newstypes={newstypes} />
           ) : (
-            <p>loading</p>
+            <NewsTypesLoading />
           )}
         </div>
         <div className="mt-3">
-          {props.targetgroups ? (
-            <TargerGroups targetgroups={props.targetgroups} systemname systemid />
+          {targetgroups ? (
+            <TargerGroups targetgroups={targetgroups} systemname systemid />
           ) : (
-            <p>loading</p>
+            <TargetgroupsLoading />
           )}
         </div>
       </div>
--- pages/console/[systemname]/[systemid]/home.js
+++ pages/console/[systemname]/[systemid]/home.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import axios from "axios";
-import cookie from "../../../../tools/cookie";
 import withAuth from "../../../../hoc/withAuth";
 import withLayout from "../../../../hoc/withLayoutConsole";
 
@@ -18,26 +16,7 @@ function Page({ systemname, aboutSystem }) {
 }
 
 Page.getInitialProps = async (ctx) => {
-  const aboutSystem = await fetchAboutSystem(ctx);
-  return { systemname: ctx.query.systemname, aboutSystem };
+  return { systemname: ctx.query.systemname };
 };
 
 export default withAuth(withLayout(Page));
-
-const fetchAboutSystem = async (ctx) => {
-  const query = ctx.query;
-  let aboutSystem = [];
-  await axios
-    .get(
-      `${process.env.REACT_APP_BE_PATH}/aboutsystem?systemid=${query.systemid}&systemname=${query.systemname}`,
-      {
-        headers: {
-          Authorization: "Bearer " + cookie.getJWT(ctx),
-        },
-      }
-    )
-    .then((res) => {
-      aboutSystem = res.data;
-    });
-  return aboutSystem;
-};
