1e1f432cd3f4840139795a867dbe3a3005d90c2d
panupongjkn <panupong.jkn@gmail.com>
2020-10-26 07:56:54 +0700

    ⚡️ use skeleton create news page

--- components/Console/System/News/CreateNews/CreateNewsPage.js
+++ components/Console/System/News/CreateNews/CreateNewsPage.js
@@ -1,11 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CreateNewsContext } from "../../../../../store/CreateNewsProvider";
+import axios from "axios";
+import cookie from "../../../../../tools/cookie";
+import { useRouter } from "next/router";
 
 import Form from "./Form/Form";
 import Preview from "./Preview/Preview";
+import Skeleton from "./Skeleton"
 
-export default function AllNewsPage({ newstypes }) {
+export default function AllNewsPage() {
   const { step } = useContext(CreateNewsContext);
+
+  const router = useRouter();
+  const { systemname, systemid } = router.query;
+  const [newstypes, setNewstypes] = useState(null);
+
   const Step = () => {
     switch (step) {
       case 1:
@@ -15,10 +24,43 @@ export default function AllNewsPage({ newstypes }) {
     }
   };
 
+  useEffect(() => {
+    fetchNewsTypes();
+  }, []);
+
+  const setNewsType = (data) => {
+    let newstypes = [];
+    for (let index = 0; index < data.length; index++) {
+      let newstype1 = data[index];
+      let newstype2 = {
+        id: newstype1.ID,
+        name: newstype1.newstype_name,
+        selected: false,
+      };
+      newstypes.push(newstype2);
+    }
+    return newstypes;
+  };
+
+  const fetchNewsTypes = () => {
+    axios
+      .get(
+        `${process.env.REACT_APP_BE_PATH}/news/newstype/all?systemid=${systemid}`,
+        {
+          headers: {
+            Authorization: "Bearer " + cookie.getJWT(),
+          },
+        }
+      )
+      .then(async (res) => {
+        setNewstypes(setNewsType(res.data));
+      });
+  };
+
   return (
     <div className="container mt-3 mb-5">
       <h1>Create News Page</h1>
-      <div className="mt-3">{Step()}</div>
+      {newstypes ? <div className="mt-3">{Step()}</div> : <div><Skeleton /></div>}
     </div>
   );
 }
--- pages/console/[systemname]/[systemid]/news/createnews.js
+++ pages/console/[systemname]/[systemid]/news/createnews.js
@@ -1,63 +1,27 @@
 import Head from "next/head";
-import axios from "axios";
-import cookie from "../../../../../tools/cookie";
 import { CreateNewsProvider } from "../../../../../store/CreateNewsProvider";
-import withAuth from "../../../../../hoc/withAuth"
-import withLayout from "../../../../../hoc/withLayoutConsole"
+import withAuth from "../../../../../hoc/withAuth";
+import withLayout from "../../../../../hoc/withLayoutConsole";
 
 import Page from "../../../../../components/Console/System/News/CreateNews/CreateNewsPage";
 
-function CreateNewsPage({ systemname, newstypes }) {
+function CreateNewsPage({ systemname }) {
   return (
     <>
       <Head>
         <title>{systemname} - NMS</title>
       </Head>
       <CreateNewsProvider>
-        <Page newstypes={newstypes} />
+        <Page />
       </CreateNewsProvider>
     </>
   );
 }
 
-const setNewsType = (data) => {
-  let newstypes = [];
-  for (let index = 0; index < data.length; index++) {
-    let newstype1 = data[index];
-    let newstype2 = {
-      id: newstype1.ID,
-      name: newstype1.newstype_name,
-      selected: false,
-    };
-    newstypes.push(newstype2);
-  }
-  return newstypes;
-};
-
-const fetchNewsTypes = async (ctx) => {
-  const query = ctx.query;
-  let newstypes = [];
-  await axios
-    .get(
-      `${process.env.REACT_APP_BE_PATH}/news/newstype/all?systemid=${query.systemid}`,
-      {
-        headers: {
-          Authorization: "Bearer " + cookie.getJWT(ctx),
-        },
-      }
-    )
-    .then(async (res) => {
-      newstypes = await setNewsType(res.data);
-    });
-  return newstypes;
-};
-
 CreateNewsPage.getInitialProps = async (ctx) => {
-  const newstypes = await fetchNewsTypes(ctx);
   return {
     systemname: ctx.query.systemname,
-    newstypes,
   };
 };
 
-export default withAuth(withLayout(CreateNewsPage))
+export default withAuth(withLayout(CreateNewsPage));
