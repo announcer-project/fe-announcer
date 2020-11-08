b61954a7393ba388df66dd4e49b74bf82c8348a5
panupongjkn <panupong.jkn@gmail.com>
2020-11-09 00:08:01 +0700

    🎨 use style

--- components/Console/System/Bot/AllIntent.js
+++ components/Console/System/Bot/AllIntent.js
@@ -7,6 +7,11 @@ import axios from "axios";
 import cookie from "../../../../tools/cookie";
 import Skeleton from "react-loading-skeleton";
 import Button from "../../../common/Button";
+// import styled from "styled-components"
+
+// const IntentBar = styled.div`
+
+// `
 
 export default function AllConnectPage() {
   const router = useRouter();
@@ -14,35 +19,40 @@ export default function AllConnectPage() {
   const [loading, setLoading] = useState(false);
   const [intents, setIntents] = useState(null);
 
-  // useEffect(() => {
-  //   fetchAllIntent();
-  // });
+  useEffect(() => {
+    fetchAllIntent();
+  }, []);
 
-  // const fetchAllIntent = async () => {
-  //   await axios
-  //     .get(
-  //       `${process.env.REACT_APP_BE_PATH}/dialogflow/intent/list?systemid=${systemid}`,
-  //       {
-  //         headers: {
-  //           Authorization: "Bearer " + cookie.getJWT(),
-  //         },
-  //       }
-  //     )
-  //     .then((res) => {
-  //       setIntents(res.data);
-  //     });
-  // };
+  const fetchAllIntent = async () => {
+    await axios
+      .get(
+        `${process.env.REACT_APP_BE_PATH}/dialogflow/intent/list?systemid=${systemid}`,
+        {
+          headers: {
+            Authorization: "Bearer " + cookie.getJWT(),
+          },
+        }
+      )
+      .then((res) => {
+        setIntents(res.data);
+      });
+  };
 
   return (
     <div>
-      <div className="d-flex justify-content-between">
-        <h2>Intents</h2>
+      <div className="mb-3 d-flex justify-content-between">
+        <h2 className="mt-2">Intents</h2>
         <Button>Create Intent</Button>
       </div>
       {intents ? (
         <div>
           {intents.map((intent, key) => {
-            return <div>{intent.display_name}</div>;
+            return (
+              <div key={key} className="border p-2 mb-1 d-flex justify-content-between">
+                <span className="mt-2">{intent.display_name}</span>
+                <Button>Manage</Button>
+              </div>
+            );
           })}
         </div>
       ) : (
