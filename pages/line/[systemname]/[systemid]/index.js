5315e8e2a2a3f0a1d9f592ed93514ea70f63b6d0
panupongjkn <panupong.jkn@gmail.com>
2020-11-11 20:58:41 +0700

    center page

---
+++ pages/line/[systemname]/[systemid]/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import dynamic from "next/dynamic";
+
+const LineLiff = dynamic(
+  () => {
+    return import("../../../../components/Line/FetchDataLine");
+  },
+  { ssr: false }
+);
+
+export default function LiffPage() {
+  return <LineLiff />
+}
