2d3da618f4ab774b9342425a96bc23abca0410ba
panupongjkn <panupong.jkn@gmail.com>
2020-11-30 01:57:24 +0700

    update howto

--- pages/howto/chatbot.js
+++ pages/howto/chatbot.js
@@ -2,20 +2,8 @@ import Header from "../../components/Howto/Header";
 function Chatbot() {
   return (
     <div>
-      <Header />
-      <div className="container py-4">
-        <b>วิธีใช้ แชทบอท</b>
-        <p>
-          คุณสามารถพิมพ์ชื่อของประเภทข่าวในช่องแชทได้ เพื่อรับข่าวสารประเภทนั้นๆ
-        </p>
-        <b>ภาพประกอบ</b>
-        <div>
-          1.พิมพ์ชื่อของประเภทข่าว
-          <img className="w-100" src="/img/howto/chatbot_1.png"/>
-          2.แชทบอทส่งข่าวสารประเภทนั้นมาให้
-          <img className="w-100 mt-2" src="/img/howto/chatbot_2.png"/>
-        </div>
-      </div>
+      {/* <Header /> */}
+      <img className="w-100" src="/img/howto/chatbot_3.png" />
     </div>
   );
 }
